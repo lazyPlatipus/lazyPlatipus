69f42a6926630fb57ba3cf9b0372479824b91fce
lazyPlatipus <caron.phil@hotmail.com>
2024-01-05 17:29:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fonction pour le défilement doux (smooth scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
