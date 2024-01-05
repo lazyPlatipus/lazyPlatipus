b9df51bf5b62f08c1fb170d83d15d14f056e94ef
lazyPlatipus <caron.phil@hotmail.com>
2024-01-05 17:35:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Fonction pour le défilement doux (smooth scroll)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Function to show the navigation bar after the page has loaded
+window.addEventListener('load', function () {
+  var navbar = document.getElementById('navbar');
+  navbar.classList.add('visible');
+});
